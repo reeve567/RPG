a957a4f3416091d351c3fcd75de6e0520c842cf6
reeve567 <18086052+reeve567@users.noreply.github.com>
2018-05-29 17:44:42 -0400

    up speed

--- src/main/java/me/imunsmart/rpg/command/admincommands/Speed.java
+++ src/main/java/me/imunsmart/rpg/command/admincommands/Speed.java
@@ -5,18 +5,10 @@ import org.bukkit.entity.Player;
 public class Speed {
 	
 	public static void run(Player player, String[] args) {
-	
-		if (args.length == 1) {
-			
-			try {
-				int speed = Integer.parseInt(args[0]);
-				
-				player.setFlySpeed(speed);
-				
-			} catch (Exception ignored) {
-				player.sendMessage("probelm child with arg");
-			}
-		}
+		
+		float speed = Float.parseFloat(args[0]);
+		
+		player.setFlySpeed(speed);
 	
 	
 	}
