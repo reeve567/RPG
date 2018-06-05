821a2257b50461e4fc519cce2981be447200ec4f
reeve567 <18086052+reeve567@users.noreply.github.com>
2018-06-05 11:23:24 -0400

    update?

--- src/main/java/me/imunsmart/rpg/Main.java
+++ src/main/java/me/imunsmart/rpg/Main.java
@@ -21,110 +21,109 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.omg.CORBA.PUBLIC_MEMBER;
 
 import java.lang.reflect.Field;
 import java.util.logging.Level;
 
 public class Main extends JavaPlugin {
-
-    public LootChests lc;
-    public static Main main;
-
-    private void register(Listener... listeners) {
-        for (Listener l : listeners) {
-            Bukkit.getPluginManager().registerEvents(l, this);
-        }
-    }
-
-    private void registerCommands() {
-        getLogger().log(Level.INFO, "Registered commands.");
-        new Admin(this);
-        new Default(this);
-        new Moderator(this);
-    }
-
-    private void registerEvents() {
-        getLogger().log(Level.INFO, "Registered events.");
-        register(new WorldEvents(this),
-                new PlayerEvents(this),
-                new DamageEvents(this),
-                new ChatEvents(this),
-                new ServerEvents(this),
-                new SignEvents(this),
-                new Repairing(this),
-                new Potions(this),
-                new AdminTools(),
-                new TeleportScrolls(this),
-                new NPCS(),
-		        new ItemNames()
-        );
-        new QuestManager(this);
-        new GemSpawners(this);
-        new EntityManager(this);
-        new Bank(this);
-        new RepairMenu(this);
-        new SellMenu(this);
-        new BuyMenu(this);
-        new Spawners(this);
-        new GlobalMarket(this);
-        new BanManager(this);
-        lc = new LootChests(this);
-    }
-
-    private void registerGlow() {
-        try {
-            Field f = Enchantment.class.getDeclaredField("acceptingNew");
-            f.setAccessible(true);
-            f.set(null, true);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
-            Glow glow = new Glow(999);
-            Enchantment.registerEnchantment(glow);
-        } catch (IllegalArgumentException ignored) {
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onDisable() {
-        Health.disable();
-        GemSpawners.disable();
-        EntityManager.pl = null;
-        Spawners.disable();
-        lc.disable();
-        Holograms.disable();
-        NPCS.disable();
-        QuestManager.disable();
-        SellMenu.disable();
-    }
-
-    @Override
-    public void onEnable() {
-    	main = this;
-	    this.saveDefaultConfig();
-	    this.reloadConfig();
-        registerGlow();
-        registerEvents();
-        registerCommands();
-        Nametags.setupDevTeam();
-        Nametags.setupTesterTeam();
-        new Holograms(this);
-
-        for (Player p : Bukkit.getOnlinePlayers()) {
-            Nametags.init(p);
-            QuestManager.init(p);
-            p.setCollidable(false);
-        }
-
-        new AutoBroadcaster(this);
-
-        new Stats(this);
-
-        Health.task(this);
-    }
-    
+	
+	public static Main main;
+	public LootChests lc;
+	
+	private void register(Listener... listeners) {
+		for (Listener l : listeners) {
+			Bukkit.getPluginManager().registerEvents(l, this);
+		}
+	}
+	
+	private void registerCommands() {
+		getLogger().log(Level.INFO, "Registered commands.");
+		new Admin(this);
+		new Default(this);
+		new Moderator(this);
+	}
+	
+	private void registerEvents() {
+		getLogger().log(Level.INFO, "Registered events.");
+		register(new WorldEvents(this),
+				new PlayerEvents(this),
+				new DamageEvents(this),
+				new ChatEvents(this),
+				new ServerEvents(this),
+				new SignEvents(this),
+				new Repairing(this),
+				new Potions(this),
+				new AdminTools(),
+				new TeleportScrolls(this),
+				new NPCS(),
+				new ItemNames()
+		);
+		new QuestManager(this);
+		new GemSpawners(this);
+		new EntityManager(this);
+		new Bank(this);
+		new RepairMenu(this);
+		new SellMenu(this);
+		new BuyMenu(this);
+		new Spawners(this);
+		new GlobalMarket(this);
+		new BanManager(this);
+		lc = new LootChests(this);
+	}
+	
+	private void registerGlow() {
+		try {
+			Field f = Enchantment.class.getDeclaredField("acceptingNew");
+			f.setAccessible(true);
+			f.set(null, true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			Glow glow = new Glow(999);
+			Enchantment.registerEnchantment(glow);
+		} catch (IllegalArgumentException ignored) {
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void onDisable() {
+		Health.disable();
+		GemSpawners.disable();
+		EntityManager.pl = null;
+		Spawners.disable();
+		lc.disable();
+		Holograms.disable();
+		NPCS.disable();
+		QuestManager.disable();
+		SellMenu.disable();
+	}
+	
+	@Override
+	public void onEnable() {
+		main = this;
+		this.saveDefaultConfig();
+		this.reloadConfig();
+		registerGlow();
+		registerEvents();
+		registerCommands();
+		Nametags.setupDevTeam();
+		Nametags.setupTesterTeam();
+		new Holograms(this);
+		
+		for (Player p : Bukkit.getOnlinePlayers()) {
+			Nametags.init(p);
+			QuestManager.init(p);
+			p.setCollidable(false);
+		}
+		
+		new AutoBroadcaster(this);
+		
+		new Stats(this);
+		
+		Health.task(this);
+	}
+	
 }
